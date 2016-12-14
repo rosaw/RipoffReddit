dc589114fd4ef1f005b3e492ff7376ac76f12b66
Rosa Wu <rosawu@umich.edu>
2016-12-13 21:40:44 -0500

    Adds upvote, add post functionality, and bootstrap style

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', [
-    '$scope',
-    function($scope) {
+	'$scope', 
+	function($scope) {
         $scope.test = 'Hello world!';
+
         $scope.posts = [
-        'post1',
-        'post2',
-        'post3',
-        'post4',
-        'post5'
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 2},
+        {title: 'post 3', upvotes: 15},
+        {title: 'post 4', upvotes: 12},
+        {title: 'post 5', upvotes: 8}
         ];
+
+        $scope.addPost = function(){
+        	if(!$scope.title || $scope.title === '') { return; }
+        	$scope.posts.push({
+        		title: $scope.title, 
+        		link: $scope.link,
+        		upvotes: 4});
+        	
+        	$scope.title = '';
+        	$scope.link = '';
+        };
+
+        $scope.incrementUpvotes = function(post){
+        	post.upvotes += 1;
+        }
     }
 ]);
